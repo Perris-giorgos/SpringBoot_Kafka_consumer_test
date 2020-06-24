a75c90d0708decea121135191cc0aae3fed4339e
Perris-giorgos <giorperr91@gmail.com>
2020-06-24 13:35:13 +0300

    adding second consumer

--- src/main/java/com/kafka/consumer/test/springBoot/kafka/consumer_test/config/KafkaConfiguration.java
+++ src/main/java/com/kafka/consumer/test/springBoot/kafka/consumer_test/config/KafkaConfiguration.java
@@ -1,5 +1,6 @@
 package com.kafka.consumer.test.springBoot.kafka.consumer_test.config;
 
+import com.kafka.consumer.test.springBoot.kafka.consumer_test.model.User;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,7 +22,7 @@ public class KafkaConfiguration {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.183:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.105:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -32,6 +34,24 @@ public class KafkaConfiguration {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, User> userConsumerFactory(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.105:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<String, User>(config, new StringDeserializer(),
+                new JsonDeserializer<>(User.class));
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(userConsumerFactory());
         return factory;
     }
 }
--- src/main/java/com/kafka/consumer/test/springBoot/kafka/consumer_test/listener/KafkaConsumer.java
+++ src/main/java/com/kafka/consumer/test/springBoot/kafka/consumer_test/listener/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.kafka.consumer.test.springBoot.kafka.consumer_test.listener;
 
+import com.kafka.consumer.test.springBoot.kafka.consumer_test.model.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,11 @@ public class KafkaConsumer {
     @KafkaListener(topics = "Kafka_Example")
     public void consume(String message) {
         System.out.println("Consumed message: " +message);
+    }
+
+    @KafkaListener(topics = "Kafka_Example_json",
+    containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " +user);
     }
 }
